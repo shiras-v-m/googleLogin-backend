bea434b3ad44aa162810e1a2181f998cf3d33d51
shirasvm <shiras.astabits@gmail.com>
2023-06-14 00:02:38 +0530

    login with google v1

---
+++ server.js
@@ -0,0 +1,21 @@
+const express=require('express')
+const cors=require('cors')
+
+const server=express()
+
+const PORT =3000 || process.env.PORT
+
+server.use(cors())
+server.use(express.json())
+// app.use(express.json())
+
+
+server.listen(PORT,()=>{
+    console.log(`login server started at ${PORT}`);
+})
+
+server.post('/api/login',(req,res)=>{
+    // handle post request
+    console.log(req.body);
+    res.redirect('http://localhost:4200/dashboard')
+})
